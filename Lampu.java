5c11d5e811d1c13087006f58c8be827243f792a3
randifarmana <randifarmana@gmail.com>
2020-03-17 15:09:26 +0700

    Kuis 1

---
+++ Lampu.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SmartRoomQuiz;
+
+/**
+ *
+ * @author User
+ */
+public class Lampu {
+    
+    private final Musik musik;
+    
+    
+    public Lampu(Musik musik){
+        this.musik=musik;
+    }
+    
+    public void setOn(int on){
+        //this.on=on;
+    }
+    
+    public void setTime(int time){
+        //this.time=time;
+    }     
+                                                                                                            
+    public void triggerMusik(){
+        
+        //jika lampu mati maka tidak ada musik
+        //jika jam besar atau sama dengan 22 sampai kecil dari 5 mainkan musik instrument
+        //jika jam 5 mainkan musik pop
+        
+    }
+}
+
+
---
+++ Musik.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SmartRoomQuiz;
+
+/**
+ *
+ * @author User
+ */
+public class Musik {
+    
+    public void setMusik(int type){
+       //display tipe musik yang sedang dimainkan 
+       //Type Musik off=0, pop=1, instrument=2;
+    }              
+}
